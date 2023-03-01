8451824382689c5f85e0438771320ef11f8cfcd4
angelinekwan <angeline.kwan@aiven.io>
2023-03-01 19:03:16 +0200

    add mermaid js

---
+++ app/routes/mermaid.tsx
@@ -0,0 +1,37 @@
+import { Mermaid } from "~/components/mermaid";
+
+export default function Index() {
+  return (
+    <>
+    <p>Mermaid hardcoded 1</p>
+			<Mermaid children={`
+				graph TD;
+					A-->B;
+					A-->C;
+					B-->D;
+					C-->D;
+			`} />
+			<p>Mermaid hardcoded 2</p>
+			<Mermaid children={`
+				sequenceDiagram
+				  Alice->>John: Hello John, how are you?
+				  John-->>Alice: Great!
+				  Alice-)John: See you later!		
+			`} />
+			<p>Mermaid hardcoded 3</p>
+			<Mermaid children={`
+				sequenceDiagram
+				  Alice->>John: Hello John, how are you?
+				  John-->>Alice: Great!
+				  Alice-)John: See you later!		
+			`} />
+			<p>Mermaid hardcoded 4</p>
+			<Mermaid children={`
+				sequenceDiagram
+				  Alice->>John: Hello John, how are you?
+				  John-->>Alice: Great!
+				  Alice-)John: See you later!		
+			`} />
+    </>
+  );
+}
--- remix.config.js
+++ remix.config.js
@@ -4,4 +4,13 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  serverDependenciesToBundle: [
+		/^mermaid.*/,
+		/^khroma*/,
+		/^d3*/,
+		/^lodash-es*/,
+		/^dagre-d3-es.*/,
+		/^robust-predicates*/,
+		/^internmap.*/,
+	],
 };
